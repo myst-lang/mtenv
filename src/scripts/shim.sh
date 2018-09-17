03f82dac5a9b40f68457aec52d7066afc949ecae
Jens <jens0512@osloskolen.no>
2018-09-17 22:54:28 +0200

    Added some comments in shim.sh script to clarify what's going on

--- src/scripts/shim.sh
+++ src/scripts/shim.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
+# If MTENV_HOME is not set, then set it to default, which is '~/.mtenv'"
 if [ -z $MTENV_HOME ]; then
   export MTENV_HOME="~/.mtenv"
 fi
 
 current_version=`cat $MTENV_HOME/global`
 
+# check if $MTENV_HOME/global which is supposed to hold current version is empty
 if [ -z $current_version ]; then
   echo "No version of Myst is currently active."
   echo "Run \`mtenv use <version>\` to set a global version."
@@ -15,4 +17,5 @@ fi
 export MYST_HOME=$MTENV_HOME/versions/$current_version
 
 # Proxy all arguments to the real executable
+# Use exec to ensure same process
 exec env $MYST_HOME/bin/myst $@
